262e035c21b944cd746d166dcdf6c4adb89e8fe9
ahsan159 <ahsan.05e159@student.uet.edu.pk>
2023-06-23 15:58:38 +0500

    created database and basic CURD in process

---
+++ sqlscripts/dbo.mainLedger.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[mainLedger] (
+    [id]           INT          NOT NULL identity(101,7),
+    [name]         VARCHAR (25) NOT NULL,
+    [manufacturer] VARCHAR (40) NOT NULL,
+    [supplier]     VARCHAR (40) NULL,
+    [costIn]       REAL         NULL,
+    [Cost]         REAL         NOT NULL,
+    [expiry]       DATE         NULL,
+    [formula]      VARCHAR (50) NULL,
+    [quantity]     INT          NOT NULL
+);
+
