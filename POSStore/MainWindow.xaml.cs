e2fb730ae605fc93902b1a869109dfe1a0681694
ahsan159 <ahsan.05e159@student.uet.edu.pk>
2023-06-27 15:59:30 +0500

    updated table

--- POSStore/MainWindow.xaml.cs
+++ POSStore/MainWindow.xaml.cs
@@ -20,9 +20,9 @@ namespace POSStore
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    
+
     public partial class MainWindow : Window
-    {        
+    {
         public string connectionString = "Data Source=ENG-RNR-05;Initial Catalog = DSPOS; Integrated Security = True";
         public string queryString = "SELECT * from mainLedger";
         public SqlConnection connection;
@@ -32,53 +32,48 @@ namespace POSStore
             InitializeComponent();
 
             connection = new SqlConnection(connectionString);
-            dataAdapter = new SqlDataAdapter(queryString, connectionString);            
+            dataAdapter = new SqlDataAdapter(queryString, connectionString);
             DataTable dTable = new DataTable();
             connection.Close();
             dataAdapter.Fill(dTable);
             drugLedger.DataContext = dTable.DefaultView;
-            
+
         }
 
         private void drugLedger_PreviewKeyDown(object sender, KeyEventArgs e)
         {
-            try
+            /*
+            if (e.Key == Key.Enter)
             {
-                if (e.Key == Key.Enter)
+                List<DataGridCellInfo> cells = drugLedger.SelectedCells.ToList();
+                string[] newData = new string[5];
+                for (int i = 0; i < 5; i++)
                 {
-                    DataRowView row = (DataRowView)drugLedger.SelectedItem;
-                    string s = row.Row.ItemArray[3].ToString();
-                    MessageBox.Show(s);
-                    //MessageBox.Show(drugLedger.SelectedCells.Count.ToString());
-                    //for(int i = 1; i < 5;i++) { MessageBox.Show(row[i].ToString()); }
-                    
-                    if (!(string.IsNullOrEmpty(row[1].ToString()) && string.IsNullOrEmpty(row[2].ToString()) && string.IsNullOrEmpty(row[5].ToString()) && string.IsNullOrEmpty(row[8].ToString())))
-                    {
-                        //MessageBox.Show(row.Row.ItemArray.Length.ToString());
-                        string cString = @"insert into mainLedger(name,manufacturer,Cost,quantity) values ('" +
-                         row["name"].ToString() + "','" +
-                         row["manufacturer"].ToString() + "','" +
-                         row["Cost"].ToString() + "','" +
-                         row["quantity"].ToString() + "');";
-                        MessageBox.Show(cString);
-                        SqlCommand cmd = new SqlCommand(cString, connection);
-                        cmd.Connection.Open();
-                        cmd.ExecuteNonQuery();
-                        dataAdapter = new SqlDataAdapter(queryString, connectionString);
-                        DataTable dTable = new DataTable();                        
-                        dataAdapter.Fill(dTable);
-                        drugLedger.DataContext = dTable.DefaultView;
-                        connection.Close();
+                    var cellContent = cells[i].Column.GetCellContent(cells[i].Item);
+                    TextBlock cellText = (TextBlock)cellContent;
+                    newData[i] = cellText.Text;
+                    //MessageBox.Show(newData[i] + Environment.NewLine + i.ToString());
+                }
 
-                    }
+                if (string.IsNullOrEmpty(newData[0].ToString().Trim()))
+                {
+                    string cString = @"insert into mainLedger(name,manufacturer,Cost,quantity) values ('" +
+                    newData[1].ToString() + "','" +
+                    newData[2].ToString() + "','" +
+                    newData[4].ToString() + "','" +
+                    newData[3].ToString() + "');";
+
+                    //MessageBox.Show(cString);
+                    addDatatoTable(cString);
+                }
+                else
+                {
+                    //MessageBox.Show("Possible Duplicate Entry")
                 }
             }
-            catch (Exception )
-            {
-
-            }
+            */
         }
-                private void addDatatoTable(string cString)
+        private void addDatatoTable(string cString)
         {
             // not only add data but also refresh the datagrid table
             SqlCommand cmd = new SqlCommand(cString, connection);
@@ -87,7 +82,7 @@ namespace POSStore
             dataAdapter = new SqlDataAdapter(queryString, connectionString);
             DataTable dTable = new DataTable();
             dataAdapter.Fill(dTable);
-            drugLedger.DataContext = null;            
+            drugLedger.DataContext = null;
             drugLedger.DataContext = dTable.DefaultView;
             connection.Close();
         }
@@ -103,10 +98,10 @@ namespace POSStore
             ///
             ///donot try to use selectedItem as if your are making new 
             ///entry. it will not exist and throw exception
-                        
-            List<DataGridCellInfo> cells = drugLedger.SelectedCells.ToList();            
+
+            List<DataGridCellInfo> cells = drugLedger.SelectedCells.ToList();
             string[] newData = new string[5];
-            for (int i = 1; i < 5; i++)
+            for (int i = 0; i < 5; i++)
             {
                 var cellContent = cells[i].Column.GetCellContent(cells[i].Item);
                 TextBlock cellText = (TextBlock)cellContent;
@@ -114,19 +109,26 @@ namespace POSStore
                 //MessageBox.Show(newData[i] + Environment.NewLine + i.ToString());
             }
 
-            string cString = @"insert into mainLedger(name,manufacturer,Cost,quantity) values ('" +
-            newData[1].ToString() + "','" +
-            newData[2].ToString() + "','" +
-            newData[3].ToString() + "','" +
-            newData[4].ToString() + "');";
+            if (string.IsNullOrEmpty(newData[0].ToString().Trim()))
+            {
+                string cString = @"insert into mainLedger(name,manufacturer,Cost,quantity) values ('" +
+                newData[1].ToString() + "','" +
+                newData[2].ToString() + "','" +
+                newData[4].ToString() + "','" +
+                newData[3].ToString() + "');";
 
-            //MessageBox.Show(cString);
-            addDatatoTable(cString);
+                //MessageBox.Show(cString);
+                addDatatoTable(cString);
+            }
+            else
+            {
+                //MessageBox.Show("Possible Duplicate Entry")
+            }
 
         }
 
         private void deleteEntry(object sender, RoutedEventArgs e)
-        {            
+        {
             DataRowView row = drugLedger.SelectedItem as DataRowView;
             string cString = @"DELETE FROM mainLedger WHERE id='" + row[0].ToString() + "';";
             //MessageBox.Show(cString);
--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 Point of Sale for drug/medical store
 # 22-jun-2023
 Created the basic drug class and reused persons class from previous project
-
+# 27-Jun-2023
+SQL interfacing for delete retrieve and create completed
